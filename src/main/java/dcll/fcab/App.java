ed08d37f16c26d039f3c5373a52a8f4486b968d2
Chefcitron <florent.cabric@yahoo.fr>
2016-02-17 11:10:02 +0100

    "CommitMAvenTamere"

---
+++ src/main/java/dcll/fcab/App.java
@@ -0,0 +1,13 @@
+package dcll.fcab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
